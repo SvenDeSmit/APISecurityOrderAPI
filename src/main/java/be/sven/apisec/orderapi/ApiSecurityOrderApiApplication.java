acf3ff7a62c9fab2a8961085a5bc69856045b0c8
SvenDeSmit <sven.desmit@telenet.be>
2022-08-28 18:37:25 +0200

    Second commit

---
+++ src/main/java/be/sven/apisec/orderapi/ApiSecurityOrderApiApplication.java
@@ -0,0 +1,14 @@
+package be.sven.apisec.orderapi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class ApiSecurityOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSecurityOrderApiApplication.class, args);
+	}
+}
